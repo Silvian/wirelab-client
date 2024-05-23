a9f2b637b0a8afb4462eaf60b76c5827e09426b9
Silvian Dragan <silvian.dragan@gmail.com>
2024-05-23 21:43:57 +0100

    added auto off feature

---
+++ auto_off.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from controllers import pyenergenie
+from controllers.constants import OFF
+from utils.handlers import DeviceCatalogue
+
+
+async def main():
+    device_catalogue = DeviceCatalogue()
+    devices = device_catalogue.list_devices()
+
+    for device in devices:
+        if device.get("auto"):
+            await asyncio.create_task(
+                pyenergenie.switch_device(
+                    switch=device["switch"],
+                    device_id=device["device_id"],
+                    device_type=device["type"],
+                    state=OFF,
+                )
+            )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
